bd139beb35ed970ed31e3a1f12ece48ec16fbafd
itsyogy <yithape@gmail.com>
2014-07-03 15:41:11 +1000

    added test class

---
+++ AYITest.h
@@ -0,0 +1,13 @@
+//
+//  AYITest.h
+//  Git Test
+//
+//  Created by Yogesh on 7/3/14.
+//  Copyright (c) 2014 asmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AYITest : NSObject
+
+@end
---
+++ AYITest.m
@@ -0,0 +1,13 @@
+//
+//  AYITest.m
+//  Git Test
+//
+//  Created by Yogesh on 7/3/14.
+//  Copyright (c) 2014 asmi. All rights reserved.
+//
+
+#import "AYITest.h"
+
+@implementation AYITest
+
+@end
